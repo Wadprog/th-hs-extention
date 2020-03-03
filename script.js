f2e567d5c7c3e437caa612c3b85bb8074d12b310
Wadprog <Vavalson@hotmail.com>
2020-03-03 10:31:25 -0400

    initial git

---
+++ script.js
@@ -0,0 +1,51 @@
+const createOrderLink = order => {
+  /*const val =
+    '<div> <div class="close_modal_btn" onClick="close()">x</div> <p class="infoBoXTOP"><a target ="_blank" href= https://tophatter.com/admin/invoices/' +
+    order +
+    '>order link click </a></p></div>'
+
+  document.body.innerHTML += val*/
+  const val =
+    '<li><a id ="HS_orderlink" href="#" target="_blank" rel="noreferrer noopener">Search by Order#' +
+    order +
+    ' <i class="icon-file"></i></a></li>'
+  document.getElementById('menu_custom_links').innerHTML += val
+}
+const close = () => {
+  alert('close')
+}
+const addInvoiceId = order => {
+  document.getElementById('custom-invoice-id').value = order
+}
+
+const searchOrderNumber = () => {
+  //get becon
+  let BeaconHistoryTimelineListItem = document.getElementsByClassName(
+    'c-BeaconHistoryTimelineListItem'
+  )
+  //tepm get the lat one
+  let lastBeacon =
+    BeaconHistoryTimelineListItem[BeaconHistoryTimelineListItem.length - 1]
+  let anchorTag = lastBeacon.getElementsByTagName('a')[0].innerText
+  if (anchorTag.indexOf('#')) {
+    let order = anchorTag.split('#')[1]
+    order = order.split('|')[0]
+    return parseInt(order)
+  } else return 0
+}
+
+const run = () => {
+  alert(document.domain)
+  // If the invoice id is already here we simply create a link
+  const invoiceIdField = document.getElementById('custom-invoice-id')
+  if (invoiceIdField.value === '') {
+    const order = searchOrderNumber()
+    if (order != 0) {
+      addInvoiceId(order)
+      createOrderLink(order)
+      console.log(` we found ${order}`)
+    } else console.log('we found nothing')
+  } else createOrderLink(invoiceIdField.value)
+}
+
+run()
